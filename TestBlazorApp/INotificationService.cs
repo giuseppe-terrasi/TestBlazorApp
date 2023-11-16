c48a3877e8fa10d0b61c6e263c180f7737495b2f
Giuseppe Terrasi <giuseppe.terrasi@anteria.it>
2023-11-16 16:36:37 +0100

    Add files

---
+++ TestBlazorApp/INotificationService.cs
@@ -0,0 +1,9 @@
+﻿namespace TestBlazorApp
+{
+    public interface INotificationService
+    {
+        public void NotifyLoading(bool isLoading);
+
+        public event EventHandler<bool> LoadingStateChanged;
+    }
+}
---
+++ TestBlazorApp/NotificationService.cs
@@ -0,0 +1,14 @@
+﻿
+namespace TestBlazorApp
+{
+    public class NotificationService : INotificationService
+    {
+        public event EventHandler<bool> LoadingStateChanged;
+
+        public async void NotifyLoading(bool isLoading)
+        {
+            await Task.Delay(100);
+            LoadingStateChanged?.Invoke(null, isLoading);
+        }
+    }
+}
